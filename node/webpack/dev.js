edb4065cb5c6bc14658e5c35401a17552f9d461b
Maksim Kostromin <daggerok@gmail.com>
2016-03-05 23:45:29 +0200

    moved commons into backupped vendors.js config

--- node/webpack/dev.js
+++ node/webpack/dev.js
@@ -2,34 +2,12 @@
 
 let
   context = `${__dirname}/../..`,
-  devtool = 'cheap-inline-module-source-map',
-  mainDir = `${context}/src/main`,
-  exclude = /(node_modules|bower_components)/,
-  webpack = require('webpack'),
-  definePlugin = webpack.DefinePlugin({
-    __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
-    __PROD__: JSON.stringify(JSON.parse(process.env.BUILD_PROD || 'false'))
-  })
-
-/*
-  // in the code:
-  if (__DEV__) {
-    console.warn('some extra debug logging');
-  }
-  if (__PROD__) {
-    helpdesk.notify('send crash report');
-  }
-*/
+  mainDir = `${context}/src/main`
 
 module.exports = {
   context,
-  devtool,
+  devtool: 'cheap-inline-module-source-map',
   entry: {
-    vendors: [
-      'react',
-      'react-dom',
-      'bootstrap/dist/css/bootstrap.min.css',
-    ],
     app: `${mainDir}/www/main`,
     admin: `${mainDir}/www/admin/main`
   },
@@ -37,14 +15,14 @@ module.exports = {
     path: `${mainDir}/resources/public/`,
     filename: '[name].js'
     // used to generate URLs to e.g. images
-    //publicPath: 'http://mycdn.com/'
+    //publicPath: 'http://npm mycdn.com/'
   },
   resolve: {
     extensions: ['', '.json', '.js', '.jsx']
   },
   module: {
     loaders: [
-      {test: /\.(js|jsx)$/, exclude, loader: 'babel-loader'},
+      {test: /\.(js|jsx)$/, loader: 'babel-loader', exclude: /(node_modules|bower_components)/},
       // use '!' to chain loaders
       {test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},
       {test: /\.css$/, loader: 'style-loader!css-loader'},
@@ -56,13 +34,5 @@ module.exports = {
       {test: /\.(png|jpg)$/, loader: 'url-loader?limit=5120'}
     ]
   },
-  // This will remove all modules in the vendors chunk from the app and admin chunks. (see condig.entry)
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendors',
-      filename: 'vendors.js'
-      // with this, you will link only one bundle on a page (app.js or admin.js), within vendors.js inside
-      // children: true,
-    })
-  ]
+  plugins: []
 }
