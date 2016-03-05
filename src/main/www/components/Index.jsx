8a08834148cc82bcc802fba23d1cd8a2e8d8b345
Maksim Kostromin <daggerok@gmail.com>
2016-03-05 23:33:29 +0200

    implement react routes

---
+++ src/main/www/components/Index.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import {Link} from 'react-router'
+
+export default () => (
+  <div class="container-fluid">
+    <p>Hey, u! holy routing.... this is an index!</p>
+  </div>
+)
+/*
+export default class Index extends Component {
+  render() {
+    return (
+     <div class="container-fluid">
+       <p>Hey, u! holy routing.... this is an index!</p>
+     </div>
+    )
+  }
+}
+*/
--- src/main/www/main.jsx
+++ src/main/www/main.jsx
@@ -1,6 +1,32 @@
 import './styles'
-import React from 'react'
-import {render} from 'react-dom'
-import Component from './components/Component'
 
-render(<Component />, document.getElementById('app'))
+import React, {Component} from 'react'
+import {render} from 'react-dom'
+import {Router, browserHistory, Route, IndexRoute, Link} from 'react-router'
+
+import Index from './components/Index'
+import Admin from './admin/components/Admin'
+
+export default class App extends Component {
+  render() {
+    return (
+        <div>
+          this is an App
+          {/* this will be either <Index> or <Admin> */}
+          {this.props.children}
+          <Link to="admin" class="btn btn-default">Show me admin</Link><br/>
+          <Link to="/" class="btn btn-default">Go home</Link><br/>
+        </div>
+    )
+  }
+}
+
+render(
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Index} />
+      <Route path="admin" component={Admin} />
+    </Route>
+  </Router>,
+  document.getElementById('app')
+)
